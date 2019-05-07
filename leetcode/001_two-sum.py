cef6ab71b25609e4d4196424dff2888493fef8dd
yanyahu <yahuyan@163.com>
2019-05-08 02:41:54 +0800

    001

---
+++ leetcode/001_two-sum.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int):
+        hash_map = dict()
+        for index, x in enumerate(nums):
+            if target - x in hash_map:
+                return [index, hash_map[target - x]]
+            hash_map[x] = index
