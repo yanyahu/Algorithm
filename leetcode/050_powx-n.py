50ec13c9600bcb3fc5009d737af18a7428fdae47
yanyahu <yahuyan@163.com>
2019-05-08 23:40:10 +0800

    50

--- leetcode/050_powx-n.py
+++ leetcode/050_powx-n.py
@@ -3,7 +3,7 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n < 0:
-            x = 1 / x
+            x = 1/ x
             n = -n
         result = 1
         while n:
