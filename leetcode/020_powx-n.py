8742036723ef8508eeb8de882dc4db0388126de9
yanyahu <yahuyan@163.com>
2019-05-08 23:38:58 +0800

    104

---
+++ leetcode/020_powx-n.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/powx-n/
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x = 1/ x
+            n = -n
+        result = 1
+        while n:
+            if n & 1:
+                result *= x
+            x *= x
+            n >>= 1
+        return result
