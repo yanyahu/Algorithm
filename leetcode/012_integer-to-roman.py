89ae89b31465fed8ee24f2db0bb30736440a8b27
yanyahu <yahuyan@163.com>
2019-07-31 19:02:33 +0800

    12 21

--- leetcode/012_integer-to-roman.py
+++ leetcode/012_integer-to-roman.py
@@ -45,8 +45,8 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 '''
 
 
-class Solution(object):
-    def intToRoman(self, num):
+class Solution:
+    def intToRoman(self, num: int) -> str:
         '''
         其实用到了一个进制的概念，很妙
         '''
@@ -54,6 +54,6 @@ class Solution(object):
         pair = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D",
                 900: "CM", 1000: "M"}
         for key in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
-            return_str += pair[key] * (num / key)
+            return_str += pair[key] * (num // key)
             num = num % key
         return return_str
