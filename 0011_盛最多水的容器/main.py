d286bcb8d9e6864aad28b0a7b89352455a968e72
yanyahu <yahuyan@163.com>
2020-07-11 14:13:16 +0800

    0011

---
+++ 0011_盛最多水的容器/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        L, R, width, res = 0, len(height) - 1, len(height) - 1, 0
+        for w in range(width, 0, -1):
+            if height[L] < height[R]:
+                res, L = max(res, height[L] * w), L + 1
+            else:
+                res, R = max(res, height[R] * w), R - 1
+        return res
