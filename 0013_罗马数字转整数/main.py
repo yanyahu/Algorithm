392e9bee7a1f010a97fd2bfe601f91149d265e28
yanyahu <yahuyan@163.com>
2020-07-14 12:30:48 +0800

    0013

---
+++ 0013_罗马数字转整数/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        result = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for i in s:
+            result += roman_dict[i]
+        return result
