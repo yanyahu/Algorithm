84acafb7214da3c276c9180ab1afbd1525b1abd1
yanyahu <yahuyan@163.com>
2020-09-13 14:02:35 +0800

    0083

--- 0083_删除排序链表中的重复元素/main.py
+++ 0083_删除排序链表中的重复元素/main.py
@@ -1,17 +1,6 @@
-# https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-'''
-Given a sorted linked list, delete all duplicates such that each element appear only once.
-
-Example 1:
-
-Input: 1->1->2
-Output: 1->2
-Example 2:
-
-Input: 1->1->2->3->3
-Output: 1->2->3
-'''
 
 # Definition for singly-linked list.
 class ListNode:
